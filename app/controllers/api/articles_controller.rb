efa85d2ec66cb25e1892bba6ee0988bd64931a19
arunbhalli <tomarbhalli@gmail.com>
2021-05-11 21:53:45 +0200

    Add created_at to get date of aticle(WIP)

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,6 +1,11 @@
 class Api::ArticlesController < ApplicationController
   def index
     articles = Article.all()
-    render json: {articles: articles}
+    articles.each_with_index do |article,index| 
+      articles[index].created_at = article.created_at.strftime("%F")
+      # article['created_at'] = article['created_at'].strftime('%F')
+    end
+   binding.pry
+    render json: { articles: articles }
   end
 end
