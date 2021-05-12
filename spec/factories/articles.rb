12d51ec3f602670fa189fbdfc6573755433f96c0
arunbhalli <tomarbhalli@gmail.com>
2021-05-12 20:32:21 +0200

    Add associate user in factory random methode for random email in user factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :article do
     title { "First title" }
     teaser { "some text" }
+    association :user
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { "Melon" }
     last_name { "Usk" }
-    email { "journalist@gmail.com" }
+    email { "#{rand(100000)}journalist@gmail.com" }
     password { "password" }
     password_confirmation { "password" }
     role { 1 }
