de8b500d23a3b94f3fed2605b83c22da58e6a808
Thomas <thomas@craftacademy.se>
2021-05-13 09:50:58 +0200

    renames spec file changes message to the format i like

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Article, type: :model do
   end
 
   describe 'Factory' do
-    it 'should have valid Factory' do
+    it 'is expected to have valid Factory' do
       expect(create(:article)).to be_valid
     end
   end
