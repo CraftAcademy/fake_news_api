3d64556d674ab49510180bc22f52c8d4cb2b1633
Thomas <thomas@craftacademy.se>
2021-05-13 10:54:35 +0200

    allows users into the system based on role if request originates from public-system

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,13 +1,10 @@
 class Api::SessionsController < DeviseTokenAuth::SessionsController
 
-
-
   def create 
     field = (resource_params.keys.map(&:to_sym) & resource_class.authentication_keys).first
     q_value = get_case_insensitive_field_from_resource_params(field)
     @resource = find_resource(field, q_value)
     if @resource.consumer? && params[:source] == 'admin-system'
-      # get out!
       render_create_error_bad_credentials
     else
       super
--- spec/requests/api/can_authenticate_based_on_user_role_and_client_source_spec.rb
+++ spec/requests/api/can_authenticate_based_on_user_role_and_client_source_spec.rb
@@ -2,31 +2,65 @@ RSpec.describe 'POST /api/auth/sign_in' do
   let!(:regular_user) { create(:user, role: 'consumer', email: 'ordinary_guy@random.com') }
   let!(:journalist) { create(:user, role: 'journalist', email: 'pulizer_prize_guy@random.com') }
 
-  describe 'as a consumer' do
-    before do
-      post '/api/auth/sign_in', params: {
-        email: 'ordinary_guy@random.com',
-        password: 'password',
-        source: 'admin-system'
-      }
+  describe 'admin-system' do
+    describe 'as a consumer' do
+      before do
+        post '/api/auth/sign_in', params: {
+          email: 'ordinary_guy@random.com',
+          password: 'password',
+          source: 'admin-system'
+        }
+      end
+  
+      it do
+        expect(response).to have_http_status 401
+      end
     end
-
-    it do
-      expect(response).to have_http_status 401
+  
+    describe 'as a journalist' do
+      before do
+        post '/api/auth/sign_in', params: {
+          email: 'pulizer_prize_guy@random.com',
+          password: 'password',
+          source: 'admin-system'
+        }
+      end
+  
+      it do
+        expect(response).to have_http_status 200
+      end
     end
   end
 
-  describe 'as a journalist' do
-    before do
-      post '/api/auth/sign_in', params: {
-        email: 'pulizer_prize_guy@random.com',
-        password: 'password',
-        source: 'admin-system'
-      }
+  describe 'public-system' do
+    describe 'as a consumer' do
+      before do
+        post '/api/auth/sign_in', params: {
+          email: 'ordinary_guy@random.com',
+          password: 'password',
+          source: 'public-system'
+        }
+      end
+  
+      it do
+        expect(response).to have_http_status 200
+      end
     end
-
-    it do
-      expect(response).to have_http_status 200
+  
+    describe 'as a journalist' do
+      before do
+        post '/api/auth/sign_in', params: {
+          email: 'pulizer_prize_guy@random.com',
+          password: 'password',
+          source: 'public-system'
+        }
+      end
+  
+      it do
+        expect(response).to have_http_status 200
+      end
     end
   end
+
+
 end
