8ebe34cb04528f824d13bbd59df8b9ddb11b27bf
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-19 15:15:11 +0200

    adds first tests

--- spec/requests/api/visitor/can_rate_the_article.spec.rb
+++ spec/requests/api/visitor/can_rate_the_article.spec.rb
@@ -1,5 +1,21 @@
-RSpec.describe 'PUT /api/ratings', type: :request do
+RSpec.describe 'POST /api/ratings', type: :request do
+  let(:article) { create(:article) }
+  
   describe 'successfully' do
-    
+    before do
+      post '/api/ratings',
+      params: {
+        article_id: article.id,
+        rating: 5
+      }
+    end
+
+    it 'is expected to respond with 201' do
+      expect(response).to have_http_status 201
+    end
+
+    it 'is expected to respond with a message' do
+      expect(response_json['message']).to eq 'You successfuly rated this article'
+    end
   end
 end
