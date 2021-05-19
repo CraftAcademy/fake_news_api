b0b603db261a9c3928894b1df642b66df760914e
kim_haaga <kim.haaga@live.se>
2021-05-19 19:36:08 +0200

    Add unit test for rating

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,3 +1,10 @@
 RSpec.describe Rating, type: :model do
- 
+  describe 'db table' do
+    it { is_expected.to have_db_column(:rating).of_type(:integer) }
+    it { is_expected.to have_db_column(:article_id).of_type(:integer) }
+  end
+
+  describe 'Relationship between rating and article' do
+    it { is_expected.to belong_to(:article) }
+  end
 end
