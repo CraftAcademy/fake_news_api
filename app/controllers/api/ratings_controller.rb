9bd5d09ebb3d2239e37a38509689c1ec21bd663d
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-20 13:42:51 +0200

    controller refactoring

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -1,13 +1,22 @@
 class Api::RatingsController < ApplicationController
   def create
     article = Article.find(params[:article_id])
-    rating = Rating.find_by user_id: current_user.id, article_id: params[:article_id]
-    if !rating      
-      article.ratings.create(rating: params[:rating], user_id: current_user.id)
-      render json: { message: 'You successfuly rated this article' }, status: 200
+    rating = Rating.find_by(user_id: current_user.id, article_id: params[:article_id])
+    if rating
+      rating.update(rating_params())
     else
-      article.ratings.update(rating: params[:rating], user_id: current_user.id)
-      render json: { message: 'You successfuly rated this article' }, status: 200
-    end    
+      article.ratings.create(rating_params())      
+    end
+    render_success()
+  end
+
+  private
+
+  def rating_params
+    return { rating: params[:rating], user_id: current_user.id }
+  end
+
+  def render_success
+    render json: { message: 'You successfuly rated this article' }, status: 200
   end
 end
