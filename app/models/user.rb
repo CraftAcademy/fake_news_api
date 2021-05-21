b7a918929d243b822b4ffe649553d686a3e638c3
luke <lukeperrin27@gmail.com>
2021-05-21 19:50:02 +0200

    sets up plans for subscriptions with stripe

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  enum role: { member: 1, journalist: 5 }
+  enum role: { member: 1, subscriber: 2, journalist: 5 }
 
   has_many :articles
   has_many :ratings
--- config/stripe/plans.rb
+++ config/stripe/plans.rb
@@ -1,27 +1,41 @@
-# This file contains descriptions of all your stripe plans
+Stripe.plan :yearly_subscription do |plan|
+  # plan name as it will appear on credit card statements
+  plan.name = 'Fake News 12 month subscription'
 
-# Example
-# Stripe::Plans::PRIMO #=> 'primo'
+  # amount in cents. This is 6.99
+  plan.amount = 10000
 
-# Stripe.plan :primo do |plan|
-#   # plan name as it will appear on credit card statements
-#   plan.name = 'Acme as a service PRIMO'
-#
-#   # amount in cents. This is 6.99
-#   plan.amount = 699
-#
-#   # currency to use for the plan (default 'usd')
-#   plan.currency = 'usd'
-#
-#   # interval must be either 'day', 'week', 'month' or 'year'
-#   plan.interval = 'month'
-#
-#   # only bill once every three months (default 1)
-#   plan.interval_count = 3
-#
-#   # number of days before charging customer's card (default 0)
-#   plan.trial_period_days = 30
-# end
+  plan.currency = 'sek'
+
+  plan.interval = 'month'
+
+  plan.interval_count = 12
+
+end
+
+Stripe.plan :half_year_subscription do |plan|
+  plan.name = 'Fake News 6 month subscription'
+
+  plan.amount = 11000
+
+  plan.currency = 'sek'
+
+  plan.interval = 'month'
+
+  plan.interval_count = 6
+end
+
+Stripe.plan :monthly_subscription do |plan|
+  plan.name = 'Fake News 12 month subscription'
+
+  plan.amount = 13000
+
+  plan.currency = 'sek'
+
+  plan.interval = 'month'
+
+  plan.interval_count = 1
+end
 
 # Once you have your plans defined, you can run
 #
