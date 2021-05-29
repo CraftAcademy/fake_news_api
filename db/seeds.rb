4dd8b05c7dd7ffb880f171e2835654e9e68dcb86
sevader14 <vsevolod.deriushkin@gmail.com>
2021-05-29 14:29:10 +0200

    refactors seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@ require 'uri'
 require 'faker'
 
 articles = JSON.parse(File.read('db/seed_data/articles.json'))
+backyards = JSON.parse(File.read('db/seed_data/backyards.json'))
 
 puts 'Creating journalists...'
 journalist = User.create(email: 'mrfake@fakenews.com', password: 'password', password_confirmation: 'password',
@@ -25,16 +26,11 @@ editor = User.create(email: 'editor@gmail.com', password: 'password', password_c
 
 puts 'Spreading the truth...'
 articles.each do |article|
-    article_db = Article.create(title: article['title'], teaser: article['teaser'], body: article['body'], created_at: (DateTime.now - rand(7)),
-                           category: article['category'], user_id: journalist.id, premium: [true, false].sample, published: true)
+  article_db = Article.create(title: article['title'], teaser: article['teaser'], body: article['body'], created_at: (DateTime.now - rand(7)),
+                              category: article['category'], user_id: journalist.id, premium: [true, false].sample, published: true)
   Rating.create(user_id: journalist.id, article_id: article_db.id, rating: [1, 2, 3, 4, 5].sample)
-end
-
-puts 'Attaching truthful images to articles...'
-Article.all.each_with_index do |article, index|
-  file = URI.open(image_url[index])
-
-  article.image.attach(io: file, filename: 'article_image.jpg', content_type: 'image/jpg')
+  image_file = URI.open(article['image'])
+  article_db.image.attach(io: image_file, filename: 'article_image.jpg', content_type: 'image/jpg')
 end
 
 puts 'My opinion is gospel...'
@@ -42,12 +38,8 @@ Article.where(backyard: false).each do |article|
   rand(4).times { article.comments.create(body: Faker::ChuckNorris.fact, user_id: User.where(role: 2).sample.id) }
 end
 
-
-
-# themes = %w[Science Aliens Covid Trump Cats CIA Singles]
-
-# puts 'Generating valid insights...'
-# (0...titles.count).each do |i|
-#   backyard_article = Article.create(title: titles[i], body: body[i], backyard: true, location: %w[Sweden Denmark].sample,
-#                                     theme: themes[i], user_id: subscriber.id)
-# end
+puts 'Generating valid insights...'
+backyards.each do |backyard|
+  backyard_db = Article.create(title: backyard['title'], body: backyard['body'], backyard: true, location: backyard['location'],
+                               theme: backyard['theme'], user_id: subscriber.id)
+end
